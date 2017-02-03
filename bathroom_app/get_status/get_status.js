8c9a716a9425ebe184490a38fc15f9120d94755b
drumadrian <adrian.drummond@gmail.com>
2017-02-02 20:55:20 -0800

    figuring out javascript lambda function to get status of a bathroom from Dynamo......next is to integrate API Gateway proxy

--- bathroom_app/get_status/get_status.js
+++ bathroom_app/get_status/get_status.js
@@ -1,11 +1,5 @@
 'use strict';
-
-
-
-
-
-exports.handler = function(event, context, callback) {
-
+exports.handler = (event, context, callback) => {
 
 var AWS = require("aws-sdk");
 AWS.config.update({
@@ -28,21 +22,41 @@ var params = {
         "unique_id": unique_id     
     }
 };
-
-
-
-	
-   
     console.log(unique_id);
     docClient.get(params, function(err, data) {
     if (err) {
         console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
     } else {
-        console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+       //console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+        console.log("GetItem succeeded:", JSON.stringify(data, data, 2));
     }
+    
+    //build the repsonse object for API Gateway 
+    var response = {
+        statusCode: 200,
+        headers: {
+            "x-custom-header" : "my custom header value"
+        },
+        body: JSON.stringify(data)
+    };
+    console.log("response: " + JSON.stringify(response))
+
+
+    //Reference:    http://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-using-old-runtime.html#transition-to-new-nodejs-runtime
+    //Old way return 
+    // context.succeed(data);
+
+    // New way (Node.js runtime v4.3).
+    context.callbackWaitsForEmptyEventLoop = true; 
+    // callback(null, 'Success message');  
+    callback(null, data);  
+
+
 });
-    //callback(null, "some success message"){console.log("success");};
+    // callback(null, "some success message"){console.log("success");};
+    // callback(null, data);//{console.log("success");};
+
 };
 
 
-exports.handler();
+// exports.handler();
