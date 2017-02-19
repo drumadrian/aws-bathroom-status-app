41eb9e53685960e938af0d4bdc29c0d9b18ea55d
drumadrian <adrian.drummond@gmail.com>
2017-02-18 21:56:34 -0800

    Work in progress with Check status using Alexa working....time for a DJ break

--- bathroom_app/alexa/skill service/AlexaBathroomLambdaFunction.py
+++ bathroom_app/alexa/skill service/AlexaBathroomLambdaFunction.py
@@ -8,6 +8,8 @@ http://amzn.to/1LGWsLG
 """
 
 from __future__ import print_function
+from boto3 import client as boto3_client
+import json
 
 
 # --------------- Helpers that build all of the responses ----------------------
@@ -50,13 +52,17 @@ def get_welcome_response():
 
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = "Welcome to the Alexa Skills Kit sample. " \
-                    "Please tell me your favorite color by saying, " \
-                    "my favorite color is red"
+    speech_output = "Welcome to the Alexa Bathroom App. " \
+                    "Please request an action by saying, " \
+                    "Reserve a stall, or" \
+                    "Vacate a stall, or" \
+                    "What is the status of my stall"
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
-    reprompt_text = "Please tell me your favorite color by saying, " \
-                    "my favorite color is red."
+    reprompt_text = "Please request an action by saying, " \
+                    "Reserve a stall, or" \
+                    "Vacate a stall, or" \
+                    "What is the status of my stall"
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
@@ -64,47 +70,132 @@ def get_welcome_response():
 
 def handle_session_end_request():
     card_title = "Session Ended"
-    speech_output = "Thank you for trying the Alexa Skills Kit sample. " \
-                    "Have a nice day! "
+    speech_output = "Thank you for trying the Alexa Bathroom App. " \
+                    "Have a nice day," \
+                    "and don't forget, " \
+                    "If you sprinkle when you tinkle, " \
+                    "Be a sweetie and wipe the seatie"
     # Setting this to true ends the session and exits the skill.
     should_end_session = True
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
 
 
-def create_favorite_color_attributes(favorite_color):
-    return {"favoriteColor": favorite_color}
+def create_favorite_stall_attributes(favorite_stall):
+    print("Favorite Stall has been set to: {}").format(favorite_stall)
+    return {"favoriteStall": favorite_stall}
 
 
-def set_color_in_session(intent, session):
-    """ Sets the color in the session and prepares the speech to reply to the
-    user.
+# --------------- START Intent hander functions ------------------
+
+def GetStallStatus_Intent_Function(intent, session):
+    """ Gets the status of a stall or stalls for a user.
     """
 
-    card_title = intent['name']
+    # card_title = intent['name']
+    card_title = 'Get Status of a Bathroom Stall'
     session_attributes = {}
     should_end_session = False
 
-    if 'Color' in intent['slots']:
-        favorite_color = intent['slots']['Color']['value']
-        session_attributes = create_favorite_color_attributes(favorite_color)
-        speech_output = "I now know your favorite color is " + \
-                        favorite_color + \
-                        ". You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-        reprompt_text = "You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
+    if session.get('attributes', {}) and "favoriteStall" in session.get('attributes', {}):
+        favorite_stall = session['attributes']['favoriteStall']
+    else:
+        favorite_stall = None
+
+    # if 'Color' in intent['slots']:
+    #     favorite_color = intent['slots']['Color']['value']
+    #     session_attributes = create_favorite_color_attributes(favorite_color)
+    #     speech_output = "I now know your favorite color is " + \
+    #                     favorite_color + \
+    #                     ". You can ask me your favorite color by saying, " \
+    #                     "what's my favorite color?"
+    #     reprompt_text = "You can ask me your favorite color by saying, " \
+    #                     "what's my favorite color?"
+    # else:
+    #     speech_output = "I'm not sure what your favorite color is. " \
+    #                     "Please try again."
+    #     reprompt_text = "I'm not sure what your favorite color is. " \
+    #                     "You can tell me your favorite color by saying, " \
+    #                     "my favorite color is red."
+
+    # Call the Primary back-end Lambda function to Get Status based on user's request
+    lambda_client = boto3_client('lambda', region_name='us-west-2')    
+
+    #Todo update to accept the requested stall to check.  Default to unique id F102 for now
+    message = {"unique_id": "F102"}
+
+    invocation_response = lambda_client.invoke(FunctionName="test-get-status",
+                                           InvocationType='RequestResponse',
+                                           Payload=json.dumps(message))
+
+    invocation_response_string = invocation_response['Payload'].read()
+    json_acceptable_string = invocation_response_string.replace("'", "\"")
+    
+    invocation_response_dictionary = json.loads(json_acceptable_string)
+
+    print('')
+    print("formatted and printed invocation response={}".format(invocation_response_dictionary) )
+
+    answer = invocation_response_dictionary['Item']['bstatus']
+
+    if answer == '0': 
+        print("Stall is available")
+        stallAvailability = True
+    elif answer == '1':
+        stallAvailability = False
+        print("Stall is NOT available")
+    else:
+        raise ValueError("Invalid status from test-get-status")
+
+
+    # Interpret response from Primary Lambda function
+    favorite_stall_speech_output=""
+
+    # Based on the result of the reponse from the Primary Lamnda function, build Alexa's response
+    if stallAvailability:
+        #ToDo:  Include a check to see if the favorite stall if a valid stall
+        speech_output = "Good news, the requested stall is available," + \
+                        "Would you like to reserve the stall"
+        # reprompt_text = "would you like to reserve the stall?"
+        reprompt_text = "Would you like to reserve the stall"
+        should_end_session = False
+    
+    else:
+        speech_output = "I'm sorry your stall isn't availble" \
+                        "Please try again later" \
+                        "In the future, " \
+                        "we will be able to notify you when your stall is no longer occupied and is avaiable"
+        reprompt_text = None
+        should_end_session = True
+        
+    return build_response(session_attributes, build_speechlet_response(
+    card_title, speech_output, reprompt_text, should_end_session))
+
+
+
+
+def SetStatusToOccupied_Intent_Function(intent, session):
+    session_attributes = {}
+    reprompt_text = None
+
+    if session.get('attributes', {}) and "favoriteStall" in session.get('attributes', {}):
+        favorite_stall = session['attributes']['favoriteStall']
+        speech_output = "Your favorite color is " + favorite_color + \
+                        ". Goodbye."
+        should_end_session = True
     else:
         speech_output = "I'm not sure what your favorite color is. " \
-                        "Please try again."
-        reprompt_text = "I'm not sure what your favorite color is. " \
-                        "You can tell me your favorite color by saying, " \
-                        "my favorite color is red."
+                        "You can say, my favorite color is red."
+        should_end_session = False
+
+    # Setting reprompt_text to None signifies that we do not want to reprompt
+    # the user. If the user does not respond or says something that is not
+    # understood, the session will end.
     return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
+        intent['name'], speech_output, reprompt_text, should_end_session))
 
 
-def get_color_from_session(intent, session):
+def SetStatusToVacant_Intent_Function(intent, session):
     session_attributes = {}
     reprompt_text = None
 
@@ -125,12 +216,15 @@ def get_color_from_session(intent, session):
         intent['name'], speech_output, reprompt_text, should_end_session))
 
 
+# --------------- END Intent hander functions ------------------
+
+
 # --------------- Events ------------------
 
 def on_session_started(session_started_request, session):
     """ Called when the session starts """
 
-    print("on_session_started requestId=" + session_started_request['requestId']
+    print("on_session_started: requestId=" + session_started_request['requestId']
           + ", sessionId=" + session['sessionId'])
 
 
@@ -139,7 +233,7 @@ def on_launch(launch_request, session):
     want
     """
 
-    print("on_launch requestId=" + launch_request['requestId'] +
+    print("on_launch: requestId=" + launch_request['requestId'] +
           ", sessionId=" + session['sessionId'])
     # Dispatch to your skill's launch
     return get_welcome_response()
@@ -148,23 +242,76 @@ def on_launch(launch_request, session):
 def on_intent(intent_request, session):
     """ Called when the user specifies an intent for this skill """
 
-    print("on_intent requestId=" + intent_request['requestId'] +
+    print("on_intent: requestId=" + intent_request['requestId'] +
           ", sessionId=" + session['sessionId'])
 
     intent = intent_request['intent']
     intent_name = intent_request['intent']['name']
 
     # Dispatch to your skill's intent handlers
-    if intent_name == "MyColorIsIntent":
-        return set_color_in_session(intent, session)
-    elif intent_name == "WhatsMyColorIntent":
-        return get_color_from_session(intent, session)
+    if intent_name == "GetStallStatus":
+        return GetStallStatus_Intent_Function(intent, session)
+
+    elif intent_name == "SetStatusToOccupied":
+        return SetStatusToOccupied_Intent_Function(intent, session)
+
+    elif intent_name == "SetStatusToVacant":
+        return SetStatusToVacant_Intent_Function(intent, session)
+
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
+
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
         return handle_session_end_request()
+
     else:
         raise ValueError("Invalid intent")
+
+
+##INTENT SCHEMA SAMPLE
+# {
+#   "intents": [
+#     {
+#       "intent": "GetStallStatus",
+#       "slots": [
+#       {
+#         "name" : "Stall",
+#         "type": "LIST_OF_NAMES_FOR_A_STALL"
+#       }
+#      ]
+#     },
+#     {
+#       "intent": "SetStatusToOccupied",
+#       "slots": [
+#       {
+#         "name" : "Stall",
+#         "type": "LIST_OF_NAMES_FOR_A_STALL"
+#       }
+#      ]
+#     },
+#     {
+#       "intent": "SetStatusToVacant",
+#       "slots": [
+#       {
+#         "name" : "Stall",
+#         "type": "LIST_OF_NAMES_FOR_A_STALL"
+#       }
+#      ]
+#     },
+#     {
+#       "intent": "AMAZON.HelpIntent"
+#     },
+#     {
+#       "intent": "AMAZON.StopIntent"
+#     },
+#     {
+#       "intent": "AMAZON.CancelIntent"
+#     }
+#   ]
+#  }
+
+
+
 
 
 def on_session_ended(session_ended_request, session):
@@ -191,9 +338,9 @@ def lambda_handler(event, context):
     prevent someone else from configuring a skill that sends requests to this
     function.
     """
-    # if (event['session']['application']['applicationId'] !=
-    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
-    #     raise ValueError("Invalid Application ID")
+    if (event['session']['application']['applicationId'] !=
+            "amzn1.ask.skill.104905e9-d5ab-4fbc-8529-1191b993ce66"):
+        raise ValueError("Invalid Application ID")
 
     if event['session']['new']:
         on_session_started({'requestId': event['request']['requestId']},
@@ -205,4 +352,11 @@ def lambda_handler(event, context):
         return on_intent(event['request'], event['session'])
     elif event['request']['type'] == "SessionEndedRequest":
         return on_session_ended(event['request'], event['session'])
+
+
+
+
+
+
+
 
