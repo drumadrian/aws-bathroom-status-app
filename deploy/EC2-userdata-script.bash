713bce6f87d328ce2c2bc22fefd63c5e2f417279
drumadrian <adrian.drummond@gmail.com>
2017-07-12 21:18:23 -0700

    Fixed incorrect yum flag

--- deploy/EC2-userdata-script.bash
+++ deploy/EC2-userdata-script.bash
@@ -1,4 +1,4 @@
-sudo yum install -Y git
+sudo yum install -y git
 cd /home/ec2-user
 git clone https://github.com/drumadrian/aws-bathroom-status-app.git
 bash aws-bathroom-status-app/deploy/configureBathroomAppConfigInstance.bash
