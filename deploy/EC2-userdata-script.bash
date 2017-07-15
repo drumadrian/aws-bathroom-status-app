dd0af2bca5f63c1bbd61c6ab195fe3c1c1ec850a
drumadrian <adrian.drummond@gmail.com>
2017-07-14 19:00:16 -0700

    updated user data script to include newlines.  Removed file

--- deploy/EC2-userdata-script.bash
+++
@@ -1,5 +0,0 @@
-sudo yum -y install git
-cd /home/ec2-user
-git clone https://github.com/drumadrian/aws-bathroom-status-app.git
-bash aws-bathroom-status-app/deploy/configureBathroomAppConfigInstance.bash
-
