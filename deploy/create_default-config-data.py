9f1c346603d007f95b47959a34bfeb6aa841a1b0
drumadrian <adrian.drummond@gmail.com>
2017-07-21 13:33:34 -0700

    WIP: Valid updated JSON ready to test on CloudFormation

--- deploy/create_default-config-data.py
+++ deploy/create_default-config-data.py
@@ -4,26 +4,30 @@ data = {
 
     "current_config": {
         
-        "alexa_function_region": "us-east-1",
         "alexa_lambda_function_arn": "The Bathroom App for Home",
         "get_status_lambda_function_arn": "The Bathroom App",
         "set_status_lambda_function_arn": "The Bathroom App",
         "sync_dyanomo_and_s3_lambda_function_arn": "The Bathroom App",
-        "name_of_bathroom_status_system": "The Bathroom App",
-        "sns_topic_arn_for_admin": "The Bathroom App",
-        "aws_s3_bucket_name": "The Bathroom App",
-        "aws_s3_bucket_name_for_website": "The Bathroom App",
-        "root_domain": "The Bathroom App",
-        "api_url": "The Bathroom App",
-        "database_table_name": "The Bathroom App",
-        "dynamo_db_database_arn": "not_yet_set",
+        "BathroomAppAPI": "1d234",
+        "API_RootUrl": "Root URL of the API gateway",
+        "adminTopicEmailListforNotifications_arn": "123",
+        "bathroomappsuperpolicy_arn": "123",
+        "bathroomappsuperrole_arn": "123",
+        "s3awsbathroomappfiles_arn": "The Bathroom App",
+        "s3awsbathroomappwebsitebucket_arn": "The Bathroom App",
+        "tablestudygurubathrooms_name": "The Bathroom App",
+        "tablestudygurubathroomsconfig_name": "The Bathroom App",
         "system_admin_name": "not_yet_set",
         "system_admin_email_address": "not_yet_set",
+        "alexa_function_region": "us-east-1",
+        "root_domain": "The Bathroom App Domain",
+        "name_of_bathroom_status_system": "The Bathroom App",
+        "aws_s3_bucket_name": "The Bathroom App",
+        "aws_s3_bucket_name_for_website": "The Bathroom App",
         "date_of_last_update": "not_yet_set",
         "time_of_last_update": "not_yet_set"
         
   },
-
 
   "prior_config": {
 
@@ -32,14 +36,18 @@ data = {
         "get_status_lambda_function_arn": "The Bathroom App",
         "set_status_lambda_function_arn": "The Bathroom App",
         "sync_dyanomo_and_s3_lambda_function_arn": "The Bathroom App",
+        "BathroomAppAPI": "1d234",
+        "root_domain": "The Bathroom App Domain",
+        "API_RootUrl": "Root URL of the API gateway",
+        "adminTopicEmailListforNotifications_arn": "123",
+        "bathroomappsuperpolicy_arn": "123",
+        "bathroomappsuperpolicy_arn": "123",
         "name_of_bathroom_status_system": "The Bathroom App",
         "sns_topic_arn_for_admin": "The Bathroom App",
         "aws_s3_bucket_name": "The Bathroom App",
         "aws_s3_bucket_name_for_website": "The Bathroom App",
-        "root_domain": "The Bathroom App",
-        "api_url": "The Bathroom App",
-        "database_table_name": "The Bathroom App",
-        "dynamo_db_database_arn": "not_yet_set",
+        "tablestudygurubathrooms_name": "The Bathroom App",
+        "tablestudygurubathroomsconfig_name": "The Bathroom App",
         "system_admin_name": "not_yet_set",
         "system_admin_email_address": "not_yet_set",
         "date_of_last_update": "not_yet_set",
@@ -78,16 +86,6 @@ data = {
   
   ]
 }
-
-
-
-
-
-
-
-
-
-
 
 
 with open("default-config-data.json", "w") as outfile:  
